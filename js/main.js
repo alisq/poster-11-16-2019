60e76e38865925dd956fbd4eb1bfcd983edc289e
alisq <ali.shamas.qadeer@gmail.com>
2019-11-16 13:22:12 -0500

    details

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+/* this script just makes it so the #page div scales to the height of the window. Feel free to delete this is you don't need it */
 var scaleAmount = $(window).height()/($("#page").height()+30)
 console.log(scaleAmount)
 $("#page").css({
